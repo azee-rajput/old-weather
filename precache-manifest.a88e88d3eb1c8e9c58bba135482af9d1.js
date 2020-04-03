6be85772d91cb4e676287333e0b45520b7f067a4
azee-rajput <azee.rajput.7@gmail.com>
2020-04-04 01:18:16 +0500

    Updates

---
+++ precache-manifest.a88e88d3eb1c8e9c58bba135482af9d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acff6ba68609200aed70fcaddfd6c1e",
+    "url": "/old-weather/index.html"
+  },
+  {
+    "revision": "cde5bc458fb45dc2b2bb",
+    "url": "/old-weather/static/css/main.282c0366.chunk.css"
+  },
+  {
+    "revision": "7f02a9dcea5239446ae2",
+    "url": "/old-weather/static/js/2.d1971cfe.chunk.js"
+  },
+  {
+    "revision": "cde5bc458fb45dc2b2bb",
+    "url": "/old-weather/static/js/main.b2e2a264.chunk.js"
+  },
+  {
+    "revision": "524be4864d37713c55b9",
+    "url": "/old-weather/static/js/runtime-main.fa7df52b.js"
+  }
+]);
